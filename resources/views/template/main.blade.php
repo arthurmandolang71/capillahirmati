fdeb0b32148214701adf534c87141f4627447c4e
macOS <arthurmandolang71@gmail.com>
2024-03-07 16:30:10 +0800

    perbaikan header

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -8,9 +8,9 @@
     <meta name="author" content="" />
     <meta name="robots" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="TemanCaleg.com" />
-    <meta property="og:title" content="TemanCaleg.com" />
-    <meta property="og:description" content="TemanCaleg.com" />
+    <meta name="description" content="capil manado" />
+    <meta property="og:title" content="capil manado" />
+    <meta property="og:description" content="capil manado" />
     <meta property="og:image" content="{{ asset('') }}assets/images/logosementara.png" />
     <meta name="format-detection" content="telephone=no">
 
